7a68a0ee995856325effa5e5c653602028ef499d
Rut Vora <rut.special@gmail.com>
2020-06-05 10:43:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/codequest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.codequest;
+
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RelativeLayout relativeLayout = findViewById(R.id.relativeLayout);
+        TextView newText = new TextView(this);
+        newText.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        newText.setText("New Text");
+        relativeLayout.addView(newText);
+    }
+}
